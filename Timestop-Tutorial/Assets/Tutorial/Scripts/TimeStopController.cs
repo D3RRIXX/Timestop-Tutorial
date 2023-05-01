eeb18a9a86da5f6e0918e634a59fd211460e24f4
evghenii-zagaievschi <jenyazagevskii@gmail.com>
2023-05-01 13:58:25 +0300

    Added resume

--- Timestop-Tutorial/Assets/Tutorial/Scripts/TimeStopController.cs
+++ Timestop-Tutorial/Assets/Tutorial/Scripts/TimeStopController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -6,9 +7,12 @@ using UnityEngine.Rendering.Universal;
 [RequireComponent(typeof(Volume), typeof(AudioSource))]
 public class TimeStopController : MonoBehaviour
 {
+	[SerializeField] private int _timeStopDuration = 5;
 	[SerializeField] private float _targetDistortion = 0.7f;
-	[SerializeField] private float _transitionDuration = 2f;
+	[SerializeField] private float _stopTransitionDuration = 2f;
+	[SerializeField] private float _resumeTransitionDuration = 1.75f;
 	[SerializeField] private AudioClip _timeStopClip;
+	[SerializeField] private AudioClip _timeResumeClip;
 	
 	private VolumeProfile _volume;
 	private AudioSource _audioSource;
@@ -40,21 +44,40 @@ public class TimeStopController : MonoBehaviour
 		_stoppedTime = true;
 		
 		Sequence sequence = DOTween.Sequence();
-		sequence.Append(GetDistortionSequence());
-		sequence.Insert(0f, GetHueShift(_colorAdjustments.hueShift.max, _transitionDuration));
+		sequence.Append(GetDistortionSequence(_stopTransitionDuration));
+		sequence.Insert(0f, GetHueShift(_colorAdjustments.hueShift.max, _stopTransitionDuration));
 		sequence.AppendCallback(() => _colorAdjustments.saturation.value = _colorAdjustments.saturation.min);
+		sequence.OnComplete(() => StartCoroutine(ResumeTimeOnFinish()));
+	}
+
+	private IEnumerator ResumeTimeOnFinish()
+	{
+		yield return new WaitForSeconds(_timeStopDuration);
+
+		ResumeTime();
+	}
+
+	private void ResumeTime()
+	{
+		_audioSource.PlayOneShot(_timeResumeClip);
+
+		Sequence sequence = DOTween.Sequence();
+		sequence.Append(GetDistortionSequence(_resumeTransitionDuration));
+		sequence.InsertCallback(0f, () => _colorAdjustments.saturation.value = 0f);
+		sequence.Insert(0f, GetHueShift(0f, _resumeTransitionDuration));
+		sequence.OnComplete(() => _stoppedTime = false);
 	}
 
 	private Tween GetHueShift(float endValue, float duration)
 		=> DOTween.To(() => _colorAdjustments.hueShift.value, value => _colorAdjustments.hueShift.value = value, endValue, duration);
 
-	private Sequence GetDistortionSequence()
+	private Sequence GetDistortionSequence(float transitionDuration)
 	{
 		Sequence sequence = DOTween.Sequence();
 		float GetDistortion() => _lensDistortion.intensity.value;
 		void SetDistortion(float value) => _lensDistortion.intensity.value = value;
 
-		float duration = _transitionDuration / 2.5f;
+		float duration = transitionDuration / 2.5f;
 		sequence.Append(DOTween.To(GetDistortion, SetDistortion, _targetDistortion, duration));
 		sequence.Append(DOTween.To(GetDistortion, SetDistortion, -_targetDistortion, duration / 2f));
 		sequence.Append(DOTween.To(GetDistortion, SetDistortion, 0f, duration));
